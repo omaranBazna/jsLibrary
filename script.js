6ed31bfee4c05ea7c8568a0a9d075215cf102355
omaranBazna <omran4d@gmail.com>
2024-04-04 03:19:50 -0400

    add console.log

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ const SERVER_URL= "https://13ee797a-f04f-4188-bd96-7bfc59f3603e-00-4zqbc1f063fq.
 function loadKey(key,api,id){
     let el=document.getElementById(id)
     fetch(SERVER_URL+"key/"+api+"/"+key).then(res=>res.json()).then(data=>{
-
+         
+        console.log(data)
         let val=data.pair.value
         el.innerText = val
     })
