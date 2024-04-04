a0372a7ccaaa4ddde706a9a4b2bbe97be1b34312
omaranBazna <omran4d@gmail.com>
2024-04-04 03:08:33 -0400

    add loadKey function

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-let hello="Hello"
+const SERVER_URL= "https://76116eb4-4da6-405e-bcae-84551fdc3c5c-00-1dyw26i9khzqz.worf.replit.dev/"; 
+
+
+function loadKey(key,api,id){
+    let el=document.getElementById(id)
+    fetch(SERVER_URL+"key/"+api+"/"+key).then(res=>res.json()).then(data=>{
+
+        let val=data.pair.value
+        el.innerText = val
+    })
+
+}
+
+
+
