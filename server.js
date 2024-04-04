9333a01977235624d0b9e2ebc2d464b0551c0930
omaranBazna <omran4d@gmail.com>
2024-04-04 02:57:45 -0400

    serve script.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 let app=require("express")()
-
+const path=require("path")
 
 app.get("/",(req,res)=>{
-    res.sendFile("./script.js")
+    res.sendFile(path.join(__dirname,"script.js"))
 })
 
 app.listen(3000)
