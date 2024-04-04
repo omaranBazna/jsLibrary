38b3f24a37a49982b43ec1a904074fb958eb50cb
omaranBazna <omran4d@gmail.com>
2024-04-04 03:24:38 -0400

    make api global

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const SERVER_URL= "https://13ee797a-f04f-4188-bd96-7bfc59f3603e-00-4zqbc1f063fq.spock.replit.dev/"; 
 
+let projectAPI="";
 
-function loadKey(key,api,id){
+function setProjectAPI(api){
+  projectAPI = api;
+}
+function loadKey(key,id){
     let el=document.getElementById(id)
-    fetch(SERVER_URL+"key/"+api+"/"+key).then(res=>res.json()).then(data=>{
+    fetch(SERVER_URL+"key/"+projectAPI+"/"+key).then(res=>res.json()).then(data=>{
          
         console.log(data)
         let val=data.pair.value
